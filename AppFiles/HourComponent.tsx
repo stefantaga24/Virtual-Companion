d9c239c029971cc4649fa80352a5b621190d4ad8
stefantaga <42568744+stefantaga@users.noreply.github.com>
2024-06-18 18:50:25 +0200

    Pressing an hour shows the professor and room number

--- AppFiles/HourComponent.tsx
+++ AppFiles/HourComponent.tsx
@@ -1,12 +1,14 @@
 import React, { useState, } from "react";
 import { View , Text, TouchableNativeFeedback,Modal, ImageBackground} from "react-native";
+import { BlurView } from '@candlefinance/blur-view';
 
 const blackColor = "#434343";
+const beigeColor = "#F4F1E3";
 
 function HourComponent(props:any) {
     const [modalVisible,setModalVisible] = useState(false);
     return (
-        <View>
+        <View key={props.id}>
             <Modal 
                 animationType = "fade"
                 transparent = {true}
@@ -15,16 +17,28 @@ function HourComponent(props:any) {
                     setModalVisible(!modalVisible);
                 }}
             >
-                <View style={{flex:1, justifyContent:"center", alignItems:"center", backgroundColor:'rgba(0,0,0,0.8)'  }}>
-                    
+                <View style={{flex:1, justifyContent:"center", alignItems:"center", backgroundColor:"rgba(0,0,0,0.5)"}}>
+                    <View style={{width:"90%", height:"30%", backgroundColor:blackColor,borderRadius:10}}>
+                        <View style={{marginLeft:"2%",marginTop:"2%"}}>
+                            <Text style={{fontSize: 18, fontFamily:"Inter-Bold", color:beigeColor}}>
+                                {props.timePeriodString}
+                            </Text>
+                            <Text style={{fontSize: 18, fontFamily:"Inter-Bold", color:beigeColor}}>
+                                {props.subjectName}  
+                            </Text>
+                            <Text style={{fontSize: 18, fontFamily:"Inter-Bold", color:beigeColor}}>
+                                {"Prof." + " " + props.teacher_name}
+                            </Text>
+                            <Text style={{fontSize: 18, fontFamily: "Inter-Medium", color: beigeColor}}>
+                                {"Room: " + props.class_location}
+                            </Text>
+                        </View>
                         
-                    <Text style={{fontSize: 18, fontFamily: "Inter-Medium", color: blackColor}}>
-                        {props.class_location + " " + props.teacher_name}
-                    </Text>
+                    </View>
                 </View>
             </Modal>
             <TouchableNativeFeedback onPress= {() => {setModalVisible(true);}}>   
-                <View key={props.id}>
+                <View>
                     <Text style={{ color: blackColor, fontSize: 18, fontFamily: "Inter-ExtraBold" }}>
                         {props.timePeriodString}
                     </Text>
